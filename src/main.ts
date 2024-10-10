b4d25d2f20405edbdd63f65ad359d56565f2c098
Alex Fernandes <contato.alexnicolas@gmail.com>
2024-10-10 00:58:39 -0300

    feat: add log after app up

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => console.log(`Running in http://localhost:${port}/`));
 }
 bootstrap();
