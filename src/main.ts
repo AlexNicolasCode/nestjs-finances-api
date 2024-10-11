7fa8506dd23c2a5abff909df1b91602e37a2353a
Alex Fernandes <contato.alexnicolas@gmail.com>
2024-10-10 21:09:46 -0300

    feat: set classs validator config

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import type { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const port = process.env.PORT || 3000;
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, () => console.log(`Running in http://localhost:${port}/`));
 }
 bootstrap();
