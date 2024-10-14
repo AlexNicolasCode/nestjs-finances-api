55454a768cf09ee4f4f12f8959301a8fe64a09f1
Alex Fernandes <contato.alexnicolas@gmail.com>
2024-10-13 22:51:55 -0300

    refactor: code redability update

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { AuthService, BcryptService, JwtService } from "./services";
 import { UserEntity } from "src/database/entities";
 import { AuthController } from "./auth.controller";
-import { TypeOrmModule } from "@nestjs/typeorm";
 
 @Module({
     imports: [
